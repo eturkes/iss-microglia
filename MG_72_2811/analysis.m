50382d82900b71d4c4a8721c37da87c5334f3070
Emir Turkes <eturkes@bu.edu>
2020-04-26 23:32:20 -0400

    Save figures of each individual gene

--- MG_72_2811/analysis.m
+++ MG_72_2811/analysis.m
@@ -22,3 +22,30 @@ GeneName(find(strcmp(GeneName, 'Ptk2b'))) = [];
 %%%%%%%%%%%%%%%%%%%%%%%%
 
 iss_change_plot_MG(o,'Pixel', GeneName)
+
+%% Individual Genes
+clear variables
+
+load(fullfile(pwd, 'o.mat'));
+Roi = round([1, max(o.SpotGlobalYX(:,2)), 1, max(o.SpotGlobalYX(:,1))]);
+
+o.plot(o.BigDapiFile, Roi, 'Pixel');
+set(gca, 'YDir', 'reverse')
+set(gca, 'XDir', 'reverse')
+
+% For all genes.
+%%%%%%%%%%%%%%%%
+fp = fopen(fullfile('codebook_comb.txt'), 'r');
+tmp = textscan(fp, '%s %s', inf);
+GeneName = tmp{1};
+fclose(fp);
+
+for i = 1:length(GeneName)
+    iss_change_plot_individual(o,'Pixel', GeneName(i))
+
+    saveas(gcf, fullfile('figures', 'latest-software-pixelbased', ...
+        'allgenes-individual', GeneName{i}), 'fig')
+    saveas(gcf, fullfile('figures', 'latest-software-pixelbased', ...
+        'allgenes-individual', GeneName{i}), 'svg')
+end
+%%%%%%%%%%%%%%%%
