9c9ccd8e97b982604f74ac196de4fd84f8c12442
Emir Turkes <eturkes@bu.edu>
2020-07-30 16:56:52 -0400

    Fix code style inconsistencies

--- MATLAB/analysis.m
+++ MATLAB/analysis.m
@@ -67,25 +67,25 @@ saveas(gcf, fullfile('results', 'figures', 'allgenes', 'clusters', 'MG3'), 'png'
 iss_change_plot_allgenes_MG3(o,'Pixel', GeneNamesMG);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'all'), 'svg');
 
-iss_change_plot_MG(o,'Pixel', GeneNamesMG);
+iss_change_plot_MG(o, 'Pixel', GeneNamesMG);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG', 'all'), 'svg');
-iss_change_plot_MG(o,'Pixel', GeneNamesMGFilt);
+iss_change_plot_MG(o, 'Pixel', GeneNamesMGFilt);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG', 'specific'), 'svg');
-iss_change_plot_MG(o,'Pixel', GeneNamesMGFilt2);
+iss_change_plot_MG(o, 'Pixel', GeneNamesMGFilt2);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG', 'non-specific'), 'svg');
 
-iss_change_plot_MG2(o,'Pixel', GeneNamesMG);
+iss_change_plot_MG2(o, 'Pixel', GeneNamesMG);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG2', 'all'), 'svg');
-iss_change_plot_MG2(o,'Pixel', GeneNamesMGFilt);
+iss_change_plot_MG2(o, 'Pixel', GeneNamesMGFilt);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG2', 'specific'), 'svg');
-iss_change_plot_MG2(o,'Pixel', GeneNamesMGFilt2);
+iss_change_plot_MG2(o, 'Pixel', GeneNamesMGFilt2);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG2', 'non-specific'), 'svg');
 
-iss_change_plot_MG3(o,'Pixel', GeneNamesMG);
+iss_change_plot_MG3(o, 'Pixel', GeneNamesMG);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG3', 'all'), 'svg');
-iss_change_plot_MG3(o,'Pixel', GeneNamesMGFilt);
+iss_change_plot_MG3(o, 'Pixel', GeneNamesMGFilt);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG3', 'specific'), 'svg');
-iss_change_plot_MG3(o,'Pixel', GeneNamesMGFilt2);
+iss_change_plot_MG3(o, 'Pixel', GeneNamesMGFilt2);
 saveas(gcf, fullfile('results', 'figures', 'MG', 'MG3', 'non-specific'), 'svg');
 
 %% MG Clusters
@@ -144,21 +144,21 @@ for i = 1:length(k)
 end
 
 %% Individual Genes
-% For all genes.
-%%%%%%%%%%%%%%%%
+% All genes.
+%%%%%%%%%%%%
 for i = 1:length(GeneNamesAll)
     iss_change_plot_individual(o, 'Pixel', GeneNamesAll(i));
     saveas(gcf, fullfile('results', 'figures', 'allgenes', 'individual', GeneNamesAll{i}), 'svg');
 end
-%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%
 
-% For MG genes.
-%%%%%%%%%%%%%%%
+% MG genes.
+%%%%%%%%%%%
 for i = 1:length(GeneNamesMG)
     iss_change_plot_individual(o, 'Pixel', GeneNamesMG(i));
     saveas(gcf, fullfile('results', 'figures', 'MG', 'individual', GeneNamesMG{i}), 'svg');
 end
-%%%%%%%%%%%%%%%
+%%%%%%%%%%%
 
 %% Spatial Cross-correlation
 genes = {'Synpr', 'Wfs1'};
